8e7537a42362f3fb7e118a0bc256795e91582687
alex.kim <alkkim@gmail.com>
2023-03-04 12:38:56 -0800

    Created inquirer prompt for user to choose action, added switch case for each choice

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //  Dependencies
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
 
@@ -7,7 +7,7 @@ require("console.table");
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        port: 3301,
+        port: 3306,
         user: 'root',
         password: 'abc123!!',
         database: 'employees_db'
@@ -15,3 +15,53 @@ const db = mysql.createConnection(
     console.log('Connected to employees_db database.')
 );
 
+// connection prompt
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connection complete')
+    employee_prompt();
+})
+
+// Initial user interaction
+
+function employee_prompt() {
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "Choose from the following options: ",
+        choices: [
+            "View Employees",
+            "View Employees by Department",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Add Role",
+            "End"]
+    })
+    // Once the user choice has been selected, the selection choice calls the corresponding function to get the information that is needed
+    .then(function (choice){
+        switch (choice.questions) {
+            case "View Employees":
+                viewEmployees();
+                break;
+            case "View Employees by Department":
+                viewEmployeesDepartment();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Remove Employee":
+                removeEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployee();
+            case "Add Role":
+                addRole();
+                break;
+            case "End":
+                db.end();
+                break;
+        }
+    })
+};
