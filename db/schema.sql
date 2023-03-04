e8ec08b01cfb0e79a200b5d9ff567dbc6f593d7f
alex.kim <alkkim@gmail.com>
2023-03-04 11:16:02 -0800

    created schema for emplopyee database

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employees;
+
+-- Create new employee DB
+CREATE DATABASE employees;
+
+-- Use the employees DB
+USE employees;
+
+-- Creates department table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+-- Creates role table
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NULL,
+    salary DECIMAL NULL, 
+    department_id INT NULL
+);
+
+-- Creates employee table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NULL,
+    last_name VARCHAR(100) NULL,
+    role_id INT NULL,
+    manager_id INT NULL 
+);
